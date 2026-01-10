a9a8e6ca7865b4f540b1e59da6af7135a6f2256f
Karim Elmogy <55699823+karim-Elmogy@users.noreply.github.com>
2026-01-11 00:29:29 +0200

    last edit

--- resources/views/front/layout/app.blade.php
+++ resources/views/front/layout/app.blade.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Place favicon.ico in the root directory -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('front/assets/img/favicon/favicon.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('front/assets/img/favicon/favicon.ico')}}">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
